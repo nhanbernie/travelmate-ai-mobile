d9a5aa998e0889310d9db1ff744f0bb01f8b3d83
nhanbernie <ttnhanbernie@gmail.com>
2025-07-10 16:41:07 +0700

    fix: container for trip router

--- app/(main)/trips.tsx
+++ app/(main)/trips.tsx
@@ -1,8 +1,16 @@
-import { View, Text } from 'react-native';
-import React from 'react';
 import ItineraryPage from '@/features/Itinerary/ItineraryPage';
+import ScreenWrapper from '@/components/layouts/ScreenWrapper ';
 const Itinerary = () => {
-  return <ItineraryPage />;
+  return (
+    <ScreenWrapper
+      useSafeArea={{
+        top: false,
+        bottom: false,
+      }}
+    >
+      <ItineraryPage />
+    </ScreenWrapper>
+  );
 };
 
 export default Itinerary;
