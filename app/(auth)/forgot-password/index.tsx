61ca63791cddf22e451318531b474dc313538a62
nhanbernie <ttnhanbernie@gmail.com>
2025-08-05 16:10:51 +0700

    fix: ui font size

--- app/(auth)/forgot-password/index.tsx
+++ app/(auth)/forgot-password/index.tsx
@@ -13,7 +13,7 @@ export default function ForgotPasswordStep1() {
     <AuthLayout>
       <View className="flex-1 w-full justify-center px-6 py-8">
         <View className="mb-8">
-          <AppText variant="h1" className="text-center text-[#E95D77] mb-2">
+          <AppText variant="h1" className="text-center text-[#E95D77] mb-2 font-extrabold">
             {t('auth.forgotPassword.title')}
           </AppText>
           <AppText variant="subtitle" className="text-center">
--- app/(auth)/register/index.tsx
+++ app/(auth)/register/index.tsx
@@ -13,7 +13,7 @@ export default function RegisterPage() {
     <AuthLayout>
       <View className="flex-1 w-full justify-center px-6 py-8">
         <View className="mb-8">
-          <AppText variant="h1" className="text-center text-[#E95D77] mb-2">
+          <AppText variant="h1" className="text-center text-[#E95D77] mb-2 font-extrabold">
             {t('auth.register.title')}
           </AppText>
           <AppText variant="subtitle" className="text-center text-[#4B5563]">
